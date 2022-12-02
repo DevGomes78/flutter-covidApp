455191ce9f97b1427b85d259e3193eb1488f7a6f
usuario <gomes.amilson@gmail.com>
2022-12-02 04:13:30 -0300

    criado metodo _topCard

--- lib/components/top_card.dart
+++ lib/components/top_card.dart
@@ -15,77 +15,80 @@ class TopCard extends StatefulWidget {
 class _TopCardState extends State<TopCard> {
   @override
   Widget build(BuildContext context) {
+    return _topCard(context);
+  }
+
+  _topCard(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 10,
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height / 3.7,
-            width: double.infinity,
-            child: Stack(
-              children: [
-                Positioned(
-                  left: -10,
-                  bottom: -80,
-                  child: SizedBox(
-                    height: 280,
-                    child: Image.asset(
-                      ImageConstants.imageAsset,
-                      fit: BoxFit.cover,
-                    ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height / 3.7,
+          width: double.infinity,
+          child: Stack(
+            children: [
+              Positioned(
+                left: -10,
+                bottom: -80,
+                child: SizedBox(
+                  height: 280,
+                  child: Image.asset(
+                    ImageConstants.imageAsset,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                const Positioned(
-                  left: 130,
-                  bottom: 130,
+              ),
+              const Positioned(
+                left: 130,
+                bottom: 130,
+                child: Text(
+                  StringConstants.useSempreMascara,
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Positioned(
+                left: 170,
+                bottom: 100,
+                child: Text(
+                  StringConstants.utilizeAlcoolGel,
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+              const Positioned(
+                left: 170,
+                bottom: 70,
+                child: Text(
+                  StringConstants.eviteAglomeracao,
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                ),
+              ),
+              const Positioned(
+                left: 170,
+                bottom: 10,
+                child: InkWell(
+                  onTap: callUrlCovid,
                   child: Text(
-                    StringConstants.useSempreMascara,
+                    StringConstants.saibaMais + StringConstants.urlCovid,
                     style: TextStyle(
-                      fontSize: 24,
+                      fontSize: 12,
                       color: Colors.black,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                const Positioned(
-                  left: 170,
-                  bottom: 100,
-                  child: Text(
-                    StringConstants.utilizeAlcoolGel,
-                    style: TextStyle(fontSize: 20, color: Colors.black),
-                  ),
-                ),
-                const Positioned(
-                  left: 170,
-                  bottom: 70,
-                  child: Text(
-                    StringConstants.eviteAglomeracao,
-                    style: TextStyle(fontSize: 20, color: Colors.black),
-                  ),
-                ),
-                const Positioned(
-                  left: 170,
-                  bottom: 10,
-                  child: InkWell(
-                    onTap: callUrlCovid,
-                    child: Text(
-                      StringConstants.saibaMais + StringConstants.urlCovid,
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
-
+      ),
+    );
   }
 }
 
