645d7ffe10bea6d03d2ba8cbbd217ce021575e90
usuario <gomes.amilson@gmail.com>
2022-12-04 04:33:07 -0300

    refatorado

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_covid_project/controlers/covid_controller.dart';
+import 'package:flutter_covid_project/service/covid_controller.dart';
 import 'package:flutter_covid_project/views/home_page.dart';
 import 'package:provider/provider.dart';
-import 'controlers/copvid_date_controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,23 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context)=> CovidController()),
-        ChangeNotifierProvider(create: (context)=> CovidDateController()),
+        ChangeNotifierProvider(create: (context) => CovidController()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-      brightness: Brightness.dark,
+          brightness: Brightness.dark,
         ),
         home: const HomePage(),
       ),
     );
   }
 }
-
