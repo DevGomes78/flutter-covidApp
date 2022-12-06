cfb0a24237381e2dd14804a2127b950d3651cdc3
usuario <gomes.amilson@gmail.com>
2022-12-06 01:09:56 -0300

    alterado nome

--- lib/service/covid_service.dart
+++ lib/service/covid_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import '../constants/service_constants.dart';
 import '../data/models/covid_models.dart';
 
-class CovidController extends ChangeNotifier {
+class CovidService extends ChangeNotifier {
   List<Data> lista = [];
   var decodejson;
 
