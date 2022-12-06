ab1178787887ca7f09a6b36e6f750a1c9f205c17
usuario <gomes.amilson@gmail.com>
2022-12-06 01:05:20 -0300

    alterado nome

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_covid_project/service/covid_controller.dart';
+import 'package:flutter_covid_project/service/covid_service.dart';
 import 'package:flutter_covid_project/views/home_page.dart';
 import 'package:provider/provider.dart';
 
