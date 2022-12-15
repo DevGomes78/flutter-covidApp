592aa191b982f9139acdf3cc32e6996f675b57c9
amilson gomes <gomes.amilson@gmail.com>
2022-12-15 11:28:02 -0300

    alterado layout

--- lib/views/covid_page.dart
+++ lib/views/covid_page.dart
@@ -67,7 +67,7 @@ class _CovidPageState extends State<CovidPage> {
               fontWeight: FontWeight.bold,
             ),
           ),
-          const SizedBox(width: 40),
+          const SizedBox(width: 130),
           IconButton(
             onPressed: loadData,
             icon: const Icon(
@@ -82,7 +82,7 @@ class _CovidPageState extends State<CovidPage> {
 
   _lastUpdate() {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      padding: const EdgeInsets.symmetric(horizontal:20, vertical: 5),
       child: Row(
         children: [
           const Text(
