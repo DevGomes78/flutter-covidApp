c582bc92245cc16938b04a364edf0157aaf63190
amilson gomes <gomes.amilson@gmail.com>
2022-12-28 15:41:05 -0300

    alterado layout

--- lib/views/description_page.dart
+++ lib/views/description_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_covid_project/components/flag_widget.dart';
 import 'package:flutter_covid_project/constants/string_constants.dart';
-import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import '../components/flag_widget.dart';
 import '../data/models/infections.dart';
 import '../data/models/covid_models.dart';
-import 'covid_page.dart';
 
 class Description extends StatelessWidget {
   Data data;
@@ -15,18 +13,116 @@ class Description extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var state = data.state.toString();
-    return LayoutBuilder(
-      builder: (context, constraints) => Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
+    return Scaffold(
+      appBar: _buildAppBar(state),
+      body: LayoutBuilder(
+        builder: (context, constraints) => Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 10),
+                _infoDetails(),
+                monthlyInfections(),
+                graficStatus(constraints),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildAppBar(String state) {
+    return AppBar(
+      title: Text(
+        data.state.toString(),
+        style: const TextStyle(
+          fontSize: 20,
+        ),
+      ),
+      actions: [
+        Container(
+          height: 30,
+          width: 30,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50)
+          ),
+          child: FlagWidget(state: state,height: 10,),)
+      ],
+    );
+  }
+
+  _infoDetails() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 100,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10), color: Colors.white12),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 40),
-              flagInfoDetails(state, context),
-              const SizedBox(height: 10),
-              monthlyInfections(),
-              graficStatus(constraints),
-              const SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black87),
+                height: 70,
+                width: 120,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 10),
+                    const Text(
+                      StringConstants.mortes,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      data.deaths.toString(),
+                      style: const TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 20),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black87),
+                height: 70,
+                width: 120,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 10),
+                    const Text(
+                      StringConstants.casos,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(width: 80),
+                    const SizedBox(height: 10),
+                    Text(
+                      data.cases.toString(),
+                      style: const TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -51,7 +147,7 @@ class Description extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        height: 250,
+        height: 230,
         width: double.infinity,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10), color: Colors.white12),
@@ -78,152 +174,6 @@ class Description extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
-
-  flagInfoDetails(String state, BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white12,
-            ),
-            height: 350,
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 100,
-                    vertical: 20,
-                  ),
-                  child: Text(
-                    data.state.toString(),
-                    style: const TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-                FlagWidget(state: state, height: 100),
-                const SizedBox(height: 10),
-                const Divider(),
-                const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        StringConstants.mortes,
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      const SizedBox(width: 70),
-                      Text(
-                        data.deaths.toString(),
-                        style: const TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 10,
-                  ),
-                  child: Row(
-                    children: [
-                      const Text(
-                        StringConstants.casos,
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      const SizedBox(width: 80),
-                      Text(
-                        data.cases.toString(),
-                        style: const TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                  ),
-                  child: Row(
-                    children: [
-                      const Text(
-                        StringConstants.suspeitos,
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      const SizedBox(width: 45),
-                      Text(
-                        data.suspects.toString(),
-                        style: const TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                  ),
-                  child: Row(
-                    children: [
-                      const Text(
-                        StringConstants.data,
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      const SizedBox(width: 90),
-                      Text(
-                        (DateFormat(" dd/MM/yyyy")
-                            .format(DateTime.parse(data.datetime.toString()))),
-                        style: const TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            child: IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const CovidPage(),
-                  ),
-                );
-              },
-              icon: const Icon(
-                Icons.arrow_back,
-                size: 30,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
