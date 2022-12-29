13465e4f7b0b69ab82ba0940355be951c45cd7d9
amilson gomes <gomes.amilson@gmail.com>
2022-12-29 11:56:42 -0300

    alterado cores

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          brightness: Brightness.dark,
+          brightness: Brightness.light,
         ),
         home: const HomePage(),
       ),
